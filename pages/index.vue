d1f1f658f5ec1b074edfa3bf4bf92c6d3ab12f08
Muramash <alexandrebroquet.cd@gmail.com>
2020-02-12 16:15:33 +0100

    weather api connected

--- pages/index.vue
+++ pages/index.vue
@@ -5,35 +5,46 @@
       <h1 class="title">
         POC_Arcachon
       </h1>
-      <h2 class="subtitle">
-        My ultimate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <hr/>
+
+    <div>
+      <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+        <div class="mt-2">Météo de : {{ text }}</div>
+        <p>{{ weather }} - {{ wdetails }}</p>
+      <b-button variant="outline-primary" v-on:click="meteoSearch">Récupérer la météo</b-button>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import axios from 'axios'
 
 export default {
   components: {
     Logo
+  },
+  data: function (){
+    return {
+      text: "",
+      weather: "",
+      wdetails: ""
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    meteoSearch: function (event){
+      console.log(this.text)
+          axios.get("http://localhost:3001/"+this.text)
+      .then(response => {
+        this.weather = response.data.main
+        this.wdetails = response.data.description
+      })
+      .catch(function (error) {console.log(error);})
+    }
   }
 }
 </script>
@@ -53,7 +64,7 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 75px;
   color: #35495e;
   letter-spacing: 1px;
 }
